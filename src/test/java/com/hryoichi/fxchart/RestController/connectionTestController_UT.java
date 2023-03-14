aa0c5495d2dbc19cad7c2abb40c571482fd288d9
rych-hysh <ryo.robolabo@gmail.com>
2023-03-14 21:06:12 +0900

    add test for connection test

---
+++ src/test/java/com/hryoichi/fxchart/RestController/connectionTestController_UT.java
@@ -0,0 +1,15 @@
+package com.hryoichi.fxchart.RestController;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class connectionTestController_UT {
+
+    @Test
+    public void 正＿アプリの接続を確認できる(){
+        String expected = "connected.";
+        connectionTestController ctc = new connectionTestController();
+        assertThat(ctc.connectionTest()).isEqualTo(expected);
+    }
+}
