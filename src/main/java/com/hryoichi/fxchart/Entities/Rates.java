d865b428b005b3d5b926f97f2ae60bd318f9f547
rych-hysh <ryo.robolabo@gmail.com>
2023-03-17 12:06:32 +0900

    collect rates

--- src/main/java/com/hryoichi/fxchart/Entities/Rates.java
+++ src/main/java/com/hryoichi/fxchart/Entities/Rates.java
@@ -1,25 +1,23 @@
 package com.hryoichi.fxchart.Entities;
 
-import java.util.Date;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
+import java.time.LocalDateTime;
+
+@Data
 @Entity
 public class Rates {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    private Date date;
+    private LocalDateTime date;
 
-    private float rate;
+    private float askPrice;
+    private float bidPrice;
 
-    public Integer getId(){ return id; }
-
-    public Date getDate(){ return date; }
-
-    public float getRate(){ return rate; }
 }
--- src/main/java/com/hryoichi/fxchart/RestController/RatesRestController.java
+++ src/main/java/com/hryoichi/fxchart/RestController/RatesRestController.java
@@ -1,18 +1,39 @@
 package com.hryoichi.fxchart.RestController;
 
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hryoichi.fxchart.Entities.Rates;
-import com.hryoichi.fxchart.Repositories.RatesRepository;
+import com.hryoichi.fxchart.Services.RatesService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/rates")
 public class RatesRestController {
     @Autowired
-    private RatesRepository rateRepository;
+    RatesService ratesService;
+    Object res;
     @GetMapping("/all")
     public @ResponseBody Iterable<Rates> getAllRates(){
-        return rateRepository.findAll();
+        return ratesService.getAllRates();
     }
+
+    @GetMapping("/collect")
+    public @ResponseBody void cll(){
+
+        ratesService.collectCurrentUSD_JPY();
+
+    }
+
 }
