2b62f6d69edd07edd04fdfb7e891b7f85e1cb5d1
rych-hysh <ryo.robolabo@gmail.com>
2023-03-17 12:08:59 +0900

    introduce algorithm ( in progress )

---
+++ src/main/java/com/hryoichi/fxchart/Algorythms/AbstractAlgorythm.java
@@ -0,0 +1,7 @@
+package com.hryoichi.fxchart.Algorythms;
+
+abstract class AbstractAlgorythm {
+    private float fund;
+    void placeTradeNow(int lots){};
+    void closePositionNow(int lots){}
+}
---
+++ src/main/java/com/hryoichi/fxchart/Events/AlgorithmCheck.java
@@ -0,0 +1,13 @@
+package com.hryoichi.fxchart.Events;
+
+import org.springframework.context.ApplicationEvent;
+
+public class AlgorithmCheck extends ApplicationEvent {
+    public AlgorithmCheck(Object source){
+        super(source);
+    }
+
+    public float gotCurrentRate(float rate){
+        return rate;
+    }
+}
