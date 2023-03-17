d48cdc774efd4440942c9955bca8b66f5a2a58d9
rych-hysh <ryo.robolabo@gmail.com>
2023-03-18 00:21:54 +0900

    make RatesUpdated Event Async

--- src/main/java/com/hryoichi/fxchart/Services/AlgorithmManageService.java
+++ src/main/java/com/hryoichi/fxchart/Services/AlgorithmManageService.java
@@ -1,20 +1,21 @@
 package com.hryoichi.fxchart.Services;
 
-import com.hryoichi.fxchart.Entities.Simulator;
 import com.hryoichi.fxchart.Events.AlgorithmCheck;
-import com.hryoichi.fxchart.Repositories.SimulatorRepository;
+import com.hryoichi.fxchart.Repositories.SimulatorsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-public class AlgorithmManageService implements ApplicationListener<AlgorithmCheck> {
+public class AlgorithmManageService {
     @Autowired
-    SimulatorRepository simulatorRepository;
-    @Override
-    public void onApplicationEvent(AlgorithmCheck event){
+    SimulatorsRepository simulatorsRepository;
+    @EventListener
+    @Async
+    public void onRateUpdated(AlgorithmCheck event){
         final var res = event.check();
         List<Simulator> runningSimulators = simulatorRepository.getRunningSimulators();
         runningSimulators.forEach(item -> item.getUserUuid());
