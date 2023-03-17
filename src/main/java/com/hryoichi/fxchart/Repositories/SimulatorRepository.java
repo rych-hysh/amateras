3b21a9fe5f1250e37b70df99737b6baaee8ea279
rych-hysh <ryo.robolabo@gmail.com>
2023-03-17 22:35:00 +0900

    make simulator repository be able to get running simulators

--- src/main/java/com/hryoichi/fxchart/Repositories/SimulatorRepository.java
+++ src/main/java/com/hryoichi/fxchart/Repositories/SimulatorRepository.java
@@ -1,8 +1,12 @@
 package com.hryoichi.fxchart.Repositories;
 
 import com.hryoichi.fxchart.Entities.Simulator;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SimulatorRepository extends CrudRepository<Simulator, Integer> {
+import java.util.List;
 
+public interface SimulatorRepository extends CrudRepository<Simulator, Integer> {
+    @Query(value = "SELECT * FROM simulator WHERE is_running = true", nativeQuery = true, countQuery = "select 1")
+    List<Simulator> getRunningSimulators();
 }
