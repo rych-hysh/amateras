e413e48649832e143b08dfb6c8c76b4e28f48eb0
rych-hysh <ryo.robolabo@gmail.com>
2023-03-30 01:16:51 +0900

    change name

--- src/test/java/com/hryoichi/amateras/AmaterasApplicationTests.java
+++ src/test/java/com/hryoichi/amateras/AmaterasApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FxchartApplicationTests {
+class AmaterasApplicationTests {
 
 	@Test
 	void contextLoads() {
