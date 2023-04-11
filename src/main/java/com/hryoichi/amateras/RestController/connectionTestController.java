12d45d5cfd1cda264861dac4434a21ed8c3eb64c
rych-hysh <ryo.robolabo@gmail.com>
2023-04-12 01:53:47 +0900

    introduce environment

--- src/main/java/com/hryoichi/amateras/RestController/connectionTestController.java
+++ src/main/java/com/hryoichi/amateras/RestController/connectionTestController.java
@@ -1,14 +1,24 @@
 package com.hryoichi.amateras.RestController;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class connectionTestController {
 
+    @Value("${spring.profiles.active}")
+    private String prof;
+
+    @Value("${test}")
+    private String test;
 
     @GetMapping("/connection")
     public String connectionTest(){
         return "connected.";
+    }
+    @GetMapping("/profile")
+    public String profileTest(){
+        return test;
     }
 }
