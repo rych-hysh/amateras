7a7890f8806e5ee8212ccaee3d529687580acd1d
rych-hysh <ryo.robolabo@gmail.com>
2023-04-18 06:14:13 +0900

    allow cors

--- src/main/java/com/hryoichi/amateras/RestController/RatesRestController.java
+++ src/main/java/com/hryoichi/amateras/RestController/RatesRestController.java
@@ -18,11 +18,13 @@ public class RatesRestController {
     @Autowired
     RatesService ratesService;
     @GetMapping("/all")
+    @CrossOrigin
     public @ResponseBody Iterable<Rates> getAllRates(){
         return ratesService.getAllRates();
     }
 
     @GetMapping("candle")
+    @CrossOrigin
     public @ResponseBody List<RatesForCandleChartDto> getRatesForCandleDto(@RequestParam(name = "endingDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Optional<LocalDateTime> endingDate, @RequestParam(name = "numOfBar") int numOfBar, @RequestParam(name = "dataInBar") int dataInBar, @RequestParam(name = "nForSigma", required = false) Optional<Integer> nForSigma) {
         Integer n = nForSigma.orElse(20);
         LocalDateTime date = endingDate.orElse(LocalDateTime.now());
@@ -30,6 +32,7 @@ public class RatesRestController {
     }
 
     @GetMapping("/collect")
+    @CrossOrigin
     public @ResponseBody void cll(){
 
         ratesService.collectCurrentUSD_JPY();
