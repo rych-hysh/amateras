49948a8174d1d785442d86b601e629046c155d69
rych-hysh <ryo.robolabo@gmail.com>
2023-04-18 05:20:50 +0900

    get rates order by asc.

--- src/main/java/com/hryoichi/amateras/Repositories/RatesRepository.java
+++ src/main/java/com/hryoichi/amateras/Repositories/RatesRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -22,6 +21,6 @@ public interface RatesRepository extends CrudRepository<Rates, Integer> {
     @Query(value = "SELECT * FROM rates WHERE date >= :date ORDER BY date ASC LIMIT :limit", nativeQuery = true, countQuery = "select 1")
     List<Rates> getRatesAfterDate(@Param("date") LocalDateTime date, @Param("limit") int limit);
 
-    @Query(value = "SELECT * FROM rates WHERE date <= :date ORDER BY date ASC LIMIT :limit", nativeQuery = true, countQuery = "select 1")
+    @Query(value = "SELECT * FROM (SELECT * FROM rates WHERE date <= :date ORDER BY date DESC limit :limit) AS subquery ORDER BY date ASC ;", nativeQuery = true, countQuery = "select 1")
     List<Rates> getRatesBeforeDate(@Param("date") LocalDateTime date, @Param("limit") int limit);
 }
